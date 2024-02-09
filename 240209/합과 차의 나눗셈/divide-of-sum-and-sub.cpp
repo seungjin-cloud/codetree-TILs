103467282f652f28b9f2527e036065712fee29f1
seungjin22 <95003717+seungjin-cloud@users.noreply.github.com>
2024-02-09 15:53:57 +0900

    Title: 합과 차의 나눗셈, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240209/합과 차의 나눗셈/divide-of-sum-and-sub.cpp
+++ 240209/합과 차의 나눗셈/divide-of-sum-and-sub.cpp
@@ -7,8 +7,8 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int a, b;
     cin >> a >> b;
-    float res = static_cast<float>(a + b) / (a - b) * 100.0;
-    res = round(res) / 100.0;
+    float res = static_cast<float>(a + b) / (a - b);
+    res = round(res * 100.0) / 100.0;
     cout << res;
     return 0;
 }
