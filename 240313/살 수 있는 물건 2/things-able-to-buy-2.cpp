4af16210602cbe7b56ef06b44cdf7694290f73ec
seungjin22 <95003717+seungjin-cloud@users.noreply.github.com>
2024-03-13 00:34:17 +0900

    Title: 살 수 있는 물건 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240313/살 수 있는 물건 2/things-able-to-buy-2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+    if(n<500){
+        cout<<"no";
+    }else if(n<1000){
+        cout<<"pen";
+    }else if(n<3000){
+        cout<<"mask";
+    }else{
+        cout<<"book";
+    }
+    return 0;
+}
