503e37a7af74e306717fda60c8bb6c4958ae846b
seungjin22 <95003717+seungjin-cloud@users.noreply.github.com>
2024-03-13 00:25:16 +0900

    Title: 정사각형의 넓이, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240313/정사각형의 넓이/area-of-a-rectangle.cpp
+++ 240313/정사각형의 넓이/area-of-a-rectangle.cpp
@@ -7,8 +7,7 @@ inline int square_area(int n) { return n * n; }
 int main() {
     int n;
     cin >> n;
-    n = square_area(n);
-    cout << n << endl;
+    cout << square_area(n) << endl;
     if (n < 5)
     {
         cout << "tiny";
