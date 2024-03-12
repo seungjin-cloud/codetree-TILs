80803879f7ead4f93cdb1559ae78d5b6f6d94209
seungjin22 <95003717+seungjin-cloud@users.noreply.github.com>
2024-03-13 01:01:20 +0900

    Title: 3 또는 5의 배수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240313/3 또는 5의 배수/multiples-of-3-or-5.cpp
+++ 240313/3 또는 5의 배수/multiples-of-3-or-5.cpp
@@ -5,11 +5,13 @@ using namespace std;
 int main() {
     // 여기에 코드를 작성해주세요.
     int a;
+    cin >> a;
     if(a%3 == 0){
         cout<<"YES"<<endl;
     }else{
         cout<<"NO"<<endl;
     }
+
     if(a%5 == 0){
         cout<<"YES"<<endl;
     }else{
