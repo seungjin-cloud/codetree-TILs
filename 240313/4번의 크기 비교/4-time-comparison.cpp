c99e0dd3705f24572e4344274f8443eca8491782
seungjin22 <95003717+seungjin-cloud@users.noreply.github.com>
2024-03-13 01:06:35 +0900

    Title: 4번의 크기 비교, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240313/4번의 크기 비교/4-time-comparison.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a;
+    cin >> a;
+    int b,c,d,e;
+    cin >> b >> c>> d>> e;
+    if(a>b){cout<<"1";}else{cout<<"0";}
+    cout<<endl;
+    if(a>c){cout<<"1";}else{cout<<"0";}
+    cout<<endl;
+    if(a>d){cout<<"1";}else{cout<<"0";}
+    cout<<endl;
+    if(a>e){cout<<"1";}else{cout<<"0";}
+    return 0;
+}
