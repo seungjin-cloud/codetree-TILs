c58f286a8afe35c75839b574068c5cc948105fbe
seungjin22 <95003717+seungjin-cloud@users.noreply.github.com>
2024-03-21 21:30:01 +0900

    Title: 홀수만 출력하기, Time: 5ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240321/홀수만 출력하기/print-only-odd-numbers.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+    int* a = new int(n);
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    for(int i=0;i<n;i++){
+        if(a[i]%3==0){cout<<a[i]<<endl;}
+    }
+    return 0;
+}
