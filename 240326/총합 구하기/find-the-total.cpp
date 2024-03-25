f4c8a5660474b4d77abc0b3c1ecd1c3c6a4ed1b5
seungjin22 <95003717+seungjin-cloud@users.noreply.github.com>
2024-03-26 00:10:35 +0900

    Title: 총합 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240326/총합 구하기/find-the-total.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a,b,sum=0;
+    cin >> a >> b;
+    for(int i=a;i<=b;i++){
+        if(i%6==0 && i%8!=0){
+            sum += i;
+        }
+    }
+    cout << sum;
+    return 0;
+}
