2aaece5cb5503a5810e2777024ee3979e8602606
seungjin22 <95003717+seungjin-cloud@users.noreply.github.com>
2024-03-26 00:13:16 +0900

    Title: a의 배수들의 곱, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240326/a의 배수들의 곱/product-of-multiples-of-a.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a,b,mul=1;
+    cin >> a >> b;
+    for(int i=1;i<=b;i++){
+        if(i%a==0){ mul *= i;}
+    }
+    cout << mul;
+    return 0;
+}
