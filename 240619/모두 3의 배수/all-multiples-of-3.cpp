fbb67f0b2d6ecce1ddc25979b49954503ed26e11
seungjin22 <95003717+seungjin-cloud@users.noreply.github.com>
2024-06-19 00:11:17 +0900

    Title: 모두 3의 배수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240619/모두 3의 배수/all-multiples-of-3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int num;
+    for(int i=0;i<5;i++){
+        cin >> num;
+        if(num%3!=0){
+            cout<<"0";
+            return 0;
+        }
+    }
+    cout<<"1";
+    return 0;
+}
